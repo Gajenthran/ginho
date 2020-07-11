954d472632aef6f7b22a485698074b70ea9b19ef
Panchalingamoorthy Gajenthran <p.gajenthran@gmail.com>
2020-07-11 18:25:39 +0200

    Add front end for diamant.

--- client/src/config/socket.js
+++ client/src/config/socket.js
@@ -1,6 +1,7 @@
 import openSocket from 'socket.io-client';
 
-const ENDPOINT = "http://localhost:8080";
+// const ENDPOINT = "http://localhost:8080";
+const ENDPOINT = "https://protected-reef-22377.herokuapp.com/";
 
 const socket = openSocket(ENDPOINT);
 
